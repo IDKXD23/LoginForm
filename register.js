42d82bb987303ae2fa872824dc0ad5c3b3f0313e
IDKXD23 <mrharolddolera@gmail.com>
2025-03-19 11:35:53 +0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,25 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8zUSflhSfV-o312NmGzMIPIVhOiWMPNQ",
+  authDomain: "dolera-17463.firebaseapp.com",
+  projectId: "dolera-17463",
+  storageBucket: "dolera-17463.firebasestorage.app",
+  messagingSenderId: "535352398372",
+  appId: "1:535352398372:web:fd87c0ea46b73099607eb1",
+  measurementId: "G-BRLNYJQXRY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const email = document.getElementById('email').value;
+const password  = document.getElementById('password').value;
+const submit = document.getElementById('submit');
+submit.addEventListener( "click", function (event){
+  event.preventDefault()
+  alert(5)
+})

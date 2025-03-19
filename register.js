64e68521cf36110dc1c884888823903f00054673
IDKXD23 <mrharolddolera@gmail.com>
2025-03-19 18:21:36 +0800

    Update register.js

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8zUSflhSfV-o312NmGzMIPIVhOiWMPNQ",
@@ -15,11 +16,25 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-const email = document.getElementById('email').value;
-const password  = document.getElementById('password').value;
 const submit = document.getElementById('submit');
 submit.addEventListener( "click", function (event){
   event.preventDefault()
-  alert(5)
+  const email = document.getElementById('email').value;
+  const password  = document.getElementById('password').value;
+  
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("Created Account");
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+    // ..
+  });
 })
